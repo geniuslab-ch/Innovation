ef5a6e404e16748c852d2350a82e9fb0b1bc7a5b
geniuslab-ch <contact@geniuslab.ch>
2025-12-23 06:36:23 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Ce fichier JavaScript peut être utilisé pour ajouter des interactions dynamiques
+// Actuellement, il n'y a pas d'interactivité avancée mais vous pouvez y ajouter des fonctionnalités.
+
+console.log("GeniusLab Landing Page Loaded");
